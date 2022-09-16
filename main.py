97e6c659ec84dcb230656972cedb81ac20b68879
Stella <stllwong@gmail.com>
2022-09-16 00:55:01 -0700

    Title and word placeholders appear after adjusting location coordinates and font color.

--- main.py
+++ main.py
@@ -11,15 +11,20 @@ window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 flashcard = Canvas(width=800, height=526)
 card_front_img = PhotoImage(file="images/card_front.png")
 flashcard.create_image(400, 263, image=card_front_img)
+
 flashcard.config(bg=BACKGROUND_COLOR, highlightthickness=0)
-flashcard.grid(row=0, column=0)
+flashcard.grid(row=0, column=0, columnspan=2)
 
-flashcard.create_text(400, 150, text="Title", font=("Arial", 40, "italic"))
-flashcard.create_text(600, 263, text="word", font=("Arial", 60, "bold"))
+flashcard.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"), fill="black")
+flashcard.create_text(400, 263, text="word", font=("Ariel", 60, "bold"), fill="black")
 
-# back portion
-card_back_img = PhotoImage(file="images/card_back.png")
+# buttons
+cross = PhotoImage(file="images/wrong.png")
+error_button = Button(image=cross, highlightthickness=0)
+error_button.grid(row=1, column=0)
 
-
+check = PhotoImage(file="images/right.png")
+yes_button = Button(image=check, highlightthickness=0)
+yes_button.grid(row=1, column=1)
 
 window.mainloop()
