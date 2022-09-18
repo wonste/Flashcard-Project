710d516446ef812dba6c17d3b1f8725f8e7bff07
Stella Wong <wongstel@oregonstate.edu>
2022-09-18 01:18:23 -0700

    Added extra commentary to break up segments and clarify code.

--- main.py
+++ main.py
@@ -6,14 +6,15 @@ BACKGROUND_COLOR = "#B1DDC6"
 
 # import data
 data = pandas.read_csv("data/french_words.csv")
-learn_this = data.to_dict(orient="records")
+to_learn = data.to_dict(orient="records")
 
 
 def next_card():
-    current_card = random.choice(learn_this)
-    print(current_card["French"])
-    # flashcard.itemconfig(flash_title, text="French")
-    # flashcard.itemconfig(flash_word, text=current_card["French"])
+    current_card = random.choice(to_learn)
+
+    # update card with title and new word
+    canvas.itemconfig(title_card, text="French")
+    canvas.itemconfig(content_card, text=current_card["French"])
 
 
 # initialize module and set up the ui window
@@ -22,15 +23,15 @@ window.title("Deck This")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
 # create the card and front portion
-flashcard = Canvas(width=800, height=526)
+canvas = Canvas(width=800, height=526)
 card_front_img = PhotoImage(file="images/card_front.png")
-flashcard.create_image(400, 263, image=card_front_img)
+canvas.create_image(400, 263, image=card_front_img)
 
-flashcard.config(bg=BACKGROUND_COLOR, highlightthickness=0)
-flashcard.grid(row=0, column=0, columnspan=2)
+title_card = canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"), fill="black")
+content_card = canvas.create_text(400, 263, text="word", font=("Ariel", 60, "bold"), fill="black")
 
-flashcard.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"), fill="black")
-flashcard.create_text(400, 263, text="word", font=("Ariel", 60, "bold"), fill="black")
+canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
+canvas.grid(row=0, column=0, columnspan=2)
 
 # buttons
 cross = PhotoImage(file="images/wrong.png")
@@ -40,5 +41,7 @@ error_button.grid(row=1, column=0)
 check = PhotoImage(file="images/right.png")
 yes_button = Button(image=check, highlightthickness=0)
 yes_button.grid(row=1, column=1)
+
+next_card()
 
 window.mainloop()
